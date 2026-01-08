0cc564657d48bf06198a0aadc757bdbbfb2152ec
Ahmed Hasseiba <hasseiba.hasseiba90@gmail.com>
2026-01-08 03:31:37 +0200

    create custom text field

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app2/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter your note here...',
+        hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
+        border: BuildBorder(),
+        enabledBorder: BuildBorder(),
+        focusedBorder: BuildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder BuildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
