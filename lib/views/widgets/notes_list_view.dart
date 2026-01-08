0ef89d9449cbd0c42e41a559a54d87addb7c37ac
Ahmed Hasseiba <hasseiba.hasseiba90@gmail.com>
2026-01-08 02:34:08 +0200

    add floating action buttom

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -10,7 +10,7 @@ class NotesListView extends StatelessWidget {
       itemCount: 10,
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: const NoteItem(),
         );
       },
