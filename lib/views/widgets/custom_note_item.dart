e02fc1ec195925aef5501457483037a62ef330f0
Ahmed Hasseiba <hasseiba.hasseiba90@gmail.com>
2026-01-15 23:51:03 +0200

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,48 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app2/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color.fromARGB(255, 204, 188, 18),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 26, color: Colors.black),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: Text(
-                'Build your career with Flutter',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black.withOpacity(.5),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: const Color.fromARGB(255, 204, 188, 18),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 26, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Build your career with Flutter',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black.withOpacity(.5),
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.black, size: 35),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.black, size: 35),
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Text(
+                'may20 2026',
+                style: TextStyle(color: Colors.black.withOpacity(.5)),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Text(
-              'may20 2026',
-              style: TextStyle(color: Colors.black.withOpacity(.5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
