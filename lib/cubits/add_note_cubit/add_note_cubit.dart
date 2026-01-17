204439568c5441ebc13a379f0161d002cec93e96
Ahmed Hasseiba <hasseiba.hasseiba90@gmail.com>
2026-01-18 01:36:45 +0200

    add mlti bloc provider for main

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
-import 'package:meta/meta.dart';
 import 'package:notes_app2/constants.dart';
 import 'package:notes_app2/models/note_model.dart';
 
@@ -8,12 +8,12 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModel note) {
+  addNote(NoteModel note) async {
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
-      notesBox.add(note);
       emit(AddNoteSuccess());
+      await notesBox.add(note);
     } catch (e) {
       emit(AddNoteFailure(e.toString()));
     }
